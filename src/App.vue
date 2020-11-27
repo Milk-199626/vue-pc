d6fea35008a382eaf8f4aecb4eca24868009c809
Milk-199626 <137246607@qq.com>
2020-11-27 20:37:58 +0800

    feat:提交头部组件和路由

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,19 @@
 <template>
   <div>
-    <Hearder />
+    <Header />
+    <!-- 加载显示当前路由组件 -->
     <router-view></router-view>
     <Footer />
   </div>
 </template>
 
 <script>
-import Hearder from "./components/Header";
+import Header from "./components/Header";
 import Footer from "./components/Footer";
 export default {
   name: "App",
   components: {
-    Hearder,
+    Header,
     Footer,
   },
 };
--- src/components/Header/index.vue
+++ src/components/Header/index.vue
@@ -1,12 +1,110 @@
 <template>
-  <div>header...</div>
+  <!-- 头部 -->
+  <header class="header">
+    <!-- 头部的第一行 -->
+    <div class="top">
+      <div class="container">
+        <div class="loginList">
+          <p>尚品汇欢迎您！</p>
+          <p>
+            <span>请</span>
+            <router-link to="/login">登录</router-link>
+            <router-link to="/register" class="regisert">免费注册</router-link>
+          </p>
+        </div>
+        <div class="typeList">
+          <a href="###">我的订单</a>
+          <a href="###">我的购物车</a>
+          <a href="###">我的尚品汇</a>
+          <a href="###">尚品汇会员</a>
+          <a href="###">企业采购</a>
+          <a href="###">关注尚品汇</a>
+          <a href="###">合作招商</a>
+          <a href="###">商家后台</a>
+        </div>
+      </div>
+    </div>
+    <!-- 头部第二行搜索区域 -->
+    <div class="bottom">
+      <h1 class="logoArea">
+        <a href="###" class="logo" title="尚品汇">
+          <img src="./images/logo.png" alt="" />
+        </a>
+      </h1>
+      <div class="searchArea">
+        <form action="###" class="searchForm">
+          <input type="text" class="header-bottom-input" />
+          <button @click="search" class="header-bottom-button" type="button">
+            搜索
+          </button>
+        </form>
+      </div>
+    </div>
+  </header>
 </template>
 
 <script>
 export default {
-  name: "Hearder",
+  name: "Header",
+  methods: {
+    search() {
+      this.$router.push("/search");
+    },
+  },
 };
 </script>
 
 <style lang="less" scoped>
+.top {
+  background: #eaeaea;
+  height: 30px;
+  line-height: 30px;
+}
+.container {
+  width: 1200px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+}
+.loginList {
+  display: flex;
+}
+.regisert {
+  padding: 0 5px;
+  margin-left: 5px;
+  border-left: 1px solid #b3aeae;
+}
+.typeList a {
+  margin: 0 5px;
+  padding-left: 10px;
+  border-left: 1px solid #b3aeae;
+}
+.typeList a:first-child {
+  border-left: none;
+}
+.bottom {
+  width: 1200px;
+  height: 106px;
+  display: flex;
+  justify-content: space-between;
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+}
+.header-bottom-input {
+  box-sizing: border-box;
+  width: 490px;
+  height: 32px;
+  border: 2px solid #ea4a36;
+  outline: none;
+}
+.header-bottom-button {
+  height: 32px;
+  width: 68px;
+  background-color: #ea4a36;
+  color: white;
+  border: none;
+  cursor: pointer;
+  outline: none;
+}
 </style>
