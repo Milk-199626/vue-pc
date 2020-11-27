d9a74e444a57b3fd39c90941a44dc37e72c89d2a
Milk-199626 <137246607@qq.com>
2020-11-27 16:20:05 +0800

    配置路由

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,22 @@
 <template>
-  
+  <div>
+    <Hearder />
+    <router-view></router-view>
+    <Footer />
+  </div>
 </template>
 
 <script>
+import Hearder from "./components/Header";
+import Footer from "./components/Footer";
 export default {
-
-}
+  name: "App",
+  components: {
+    Hearder,
+    Footer,
+  },
+};
 </script>
 
-<style>
-
+<style lang="less" scoped>
 </style>
---
+++ src/components/Header/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>header...</div>
+</template>
+
+<script>
+export default {
+  name: "Hearder",
+};
+</script>
+
+<style lang="less" scoped>
+</style>
