fdb2114cdb4d578c0f86339edefcdbc36ee7b93b
Milk-199626 <137246607@qq.com>
2020-11-28 12:49:32 +0800

    fix:修复了编程时导航重复点击时报错

--- src/router/index.js
+++ src/router/index.js
@@ -6,8 +6,28 @@ import Login from "../views/Login";
 import Register from "../views/Register";
 import Search from "../views/Search";
 
-Vue.use(VueRouter);
+const push = VueRouter.prototype.push;
+const replace = VueRouter.prototype.replace;
+// 目的：为了让编程式导航重复点击时不报错~
+VueRouter.prototype.push = function(location, onComplete, onAbort) {
+  // console.log(VueRouter);
+  //如果客户想处理失败
+  if (onComplete && onAbort) {
+    return push.call(this, location, onComplete, onAbort);
+  }
+  // 如果客户不像处理失败，给默认值：空函数
+  return push.call(this, location, onComplete, () => {});
+};
+VueRouter.prototype.replace = function(location, onComplete, onAbort) {
+  //如果客户想处理失败
+  if (onComplete && onAbort) {
+    return replace.call(this, location, onComplete, onAbort);
+  }
+  //
+  return replace.call(this, location, onComplete, () => {});
+};
 
+Vue.use(VueRouter);
 export default new VueRouter({
   routes: [
     {
